3f68532920e6da6ee1a373b0a694bf4386f5f3f5
Antialtruist <59281488+Antialtruist@users.noreply.github.com>
2020-06-12 10:15:58 +0300

    Create Question1.java

---
+++ sprint01/PracticeTaskClassDesignEncapsulationQuiz/Question1.java
@@ -0,0 +1,8 @@
+package sprint01.PracticeTaskClassDesignEncapsulationQuiz;
+
+//		Create a Pet class with public fields name of type String and age of type int that are not initialized.
+
+public class Pet {
+    public String name;
+    public int age;
+}
